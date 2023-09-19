9cb720eaa79eff4b796dbbb768f3be49cd12acc5
ljw <ljw2017@sjtu.edu.cn>
2023-09-19 20:14:45 +0800

    first commit

---
+++ maz/maz.sh
@@ -0,0 +1,4 @@
+import sys
+sys.path.append("/media/ljw/f3b85364-e45d-4166-8db8-1cca425f188e1/zhangmo/ljwlib/")
+import softwares
+softwares.run_macs2(rep, outpath, adapter, minlen=20, utrim=0, dtrim=0, threads=6, genome_index='/home/ljw/hg19_with_bowtie2_index/hg19', qvalue=0.001):
